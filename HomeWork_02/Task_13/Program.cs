1653fec87794a744ad9a8b6616d26ce413614611
KeriMaeva <Karinchikmaeva@bk.ru>
2022-10-15 02:32:55 +0300

    решили 13 задачу

---
+++ HomeWork_02/Task_13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите метод, который выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+String ThirdNumber(int x)
+{
+    int thirdNumber = (x / 100) % 10;
+    if (thirdNumber != 0) { return thirdNumber.ToString(); }
+    else return "Третьей цифры в числе нет";
+}
+Console.WriteLine(ThirdNumber(645));
+Console.WriteLine(ThirdNumber(78));
+Console.WriteLine(ThirdNumber(32679));
