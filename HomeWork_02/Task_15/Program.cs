8ad4b9d6f65a26def0c1cf385b37eb4aee428348
KeriMaeva <Karinchikmaeva@bk.ru>
2022-10-15 02:54:20 +0300

    решили 15 задачу

---
+++ HomeWork_02/Task_15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 15: Напишите метод, который принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+String WeekDay(int x)
+{
+    if (x < 6) return x + " день недели является рабочим";
+    if (x < 8) return x + " день недели является выходным";
+    else return x + " день не является днем недели";
+}
+Console.WriteLine(WeekDay(6));
+Console.WriteLine(WeekDay(7));
+Console.WriteLine(WeekDay(1));
