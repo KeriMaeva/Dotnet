69b202f3bfd3bbf064b6658124a2ff24b8f42bc7
KeriMaeva <Karinchikmaeva@bk.ru>
2022-10-15 02:06:42 +0300

    решили 10 задачу

---
+++ HomeWork_02/Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите метод, который принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecondNumber(int x)
+{
+    int secondNumber = (x / 10) % 10;
+    return secondNumber;
+}
+Console.WriteLine(SecondNumber(456));
+Console.WriteLine(SecondNumber(782));
+Console.WriteLine(SecondNumber(918));
