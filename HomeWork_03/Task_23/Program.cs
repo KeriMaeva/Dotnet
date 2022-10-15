9add9e447dee3863fc99bd5a480cb2bf5d8b4529
KeriMaeva <Karinchikmaeva@bk.ru>
2022-10-15 08:11:10 +0300

    решили 23 задачу

---
+++ HomeWork_03/Task_23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23: Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber(string text)
+{
+    Console.Write(text + ": ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int[] GetPow(int n)
+{
+    int[] squares = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        squares[i] = (i + 1) * (i + 1) * (i + 1);
+    }
+    return squares;
+}
+int n = GetNumber("Введите число");
+System.Console.Write(n + " -> ");
+System.Console.WriteLine(String.Join(' ', GetPow(n)));
