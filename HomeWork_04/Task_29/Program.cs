e150ebcb59cd9102c6f097a08e81b704947d0ec5
KeriMaeva <Karinchikmaeva@bk.ru>
2022-10-16 08:50:59 +0300

    решили 29 задачу

---
+++ HomeWork_04/Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите метод, который задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void NewArray()
+{
+    int size = 8;
+    int[] array = new int[8];
+    Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 20);
+                Console.Write("{0} ", array[i]);
+            }
+}
+NewArray();
